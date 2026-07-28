9cb459f8359e19e957421ff10a9272729e08e9c8
adonisventuramartinez-ai <adonisventuramartinez@gmail.com>
2026-07-27 21:23:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-};
+  env: {
+    FB_APP_ID: process.env.FB_APP_ID,
+    FB_APP_SECRET: process.env.FB_APP_SECRET,
+    NEXT_PUBLIC_FB_APP_ID: process.env.NEXT_PUBLIC_FB_APP_ID,
+  },
+  // Configuración para leer archivos .env en producción
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXT_PUBLIC_FB_APP_ID: process.env.NEXT_PUBLIC_FB_APP_ID,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
